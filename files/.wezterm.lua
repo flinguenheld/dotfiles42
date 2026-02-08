77394925a122d2b4bf4cf83c1c4c6d4f95874e96
flinguen <florent@linguenheld.net>
2026-02-08 17:42:46 +0100

    Up next_theme to change wezterm

--- files/.wezterm.lua
+++ files/.wezterm.lua
@@ -9,19 +9,29 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
+config.initial_cols = 100
+config.initial_rows = 50
 
 -- or, changing the font size and color scheme.
 config.font_size = 11
-config.font = wezterm.font 'Hack Nerd Font'
-config.color_scheme = 'Batman'
+-- config.font = wezterm.font 'Hack Nerd Font'
+config.color_scheme = 'Gruvbox Dark (Gogh)'
+
+--  ---------------------------------------------------------------------------
+--  ---------------------------------------------------------------- WINDOW ---
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+config.window_padding = {
+  left   = 0,
+  right  = 0,
+  top    = 0,
+  bottom = 0,
+}
 
 --  ---------------------------------------------------------------------------
 --  -------------------------------------------------------------- KEYBINDS ---
-
 -- timeout_milliseconds defaults to 1000 and can be omitted
-config.leader = { key = 'n', mods = 'CTRL', timeout_milliseconds = 2000 }
+config.leader = { key = 't', mods = 'CTRL', timeout_milliseconds = 2000 }
 config.keys = {
   {
     key = '|',
