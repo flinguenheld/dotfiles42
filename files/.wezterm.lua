6293da100a47b57e57a3d2470f395e392fb9a710
flinguen <florent@linguenheld.net>
2026-06-22 00:00:36 +0200

    Add haskell goinfre

--- files/.wezterm.lua
+++ files/.wezterm.lua
@@ -15,7 +15,7 @@ config.initial_rows = 50
 -- or, changing the font size and color scheme.
 config.font_size = 10
 -- config.font = wezterm.font 'Hack Nerd Font'
-config.color_scheme = 'One Dark (Gogh)'
+config.color_scheme = 'Catppuccin Macchiato'
 
 --  ---------------------------------------------------------------------------
 --  ---------------------------------------------------------------- WINDOW ---
--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,20 @@
 set -x PATH $PATH ~/bin/
 set -x PATH $PATH ~/.local/bin
 
+# HASKELL ##############
+set -gx GOINFRE_DIR ~/goinfre
+
+set -x PATH $PATH $GOINFRE_DIR/.ghcup/bin
+
+set -gx GHCUP_INSTALL_BASE_PREFIX $GOINFRE_DIR
+set -gx BOOTSTRAP_HASKELL_NONINTERACTIVE 1
+
+set -gx CABAL_DIR $GOINFRE_DIR/.cabal
+set -gx CABAL_CONFIG $CABAL_DIR/config
+set -gx CABAL_STORE_DIR $CABAL_DIR/store
+set -gx CABAL_LOG_DIR $CABAL_DIR/logs
+# ######################
+
 alias la="ls -la"
 alias ea="eza -la"
 
